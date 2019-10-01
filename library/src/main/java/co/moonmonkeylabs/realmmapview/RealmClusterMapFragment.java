b0bf81cb36035b1171575deae60dc5cff0df4484
Tim Havelka <tim.havelka@gmail.com>
2019-10-01 16:21:57 -0700

    Update to androidx and sdk 29

--- library/src/main/java/co/moonmonkeylabs/realmmapview/RealmClusterMapFragment.java
+++ library/src/main/java/co/moonmonkeylabs/realmmapview/RealmClusterMapFragment.java
@@ -2,10 +2,11 @@ package co.moonmonkeylabs.realmmapview;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
